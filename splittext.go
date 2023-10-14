da7737a231ac18734bad0f57dea7bafc1b53e6ac
Francesco Alemanno <50984334+francescoalemanno@users.noreply.github.com>
2023-10-14 17:39:57 +0200

    Update splittext.go

--- splittext.go
+++ splittext.go
@@ -28,8 +28,8 @@ func (f *Fpdf) SplitText(txt string, w float64) (lines []string) {
 	l := 0
 	for i < nb {
 		c := s[i]
-		l += cw[c]
-		if unicode.IsSpace(c) || isChinese(c) {
+		l += cw[c%256]
+		if unicode.IsSpace(c) {
 			sep = i
 		}
 		if c == '\n' || l > wmax {
