567f1682d49de8bc7782a1bf9c959f2b71d26ecb
Phillip Darrall <pdarrall@papermule.co.uk>
2024-03-21 09:59:36 +0000

    Removed unused example

--- fpdf_example_test.go
+++ fpdf_example_test.go
@@ -2750,22 +2750,3 @@ func ExampleFpdf_SetModificationDate() {
 	// Output:
 	// Successfully generated pdf/Fpdf_SetModificationDate.pdf
 }
-
-func ExampleFpdf_SetWordSpacing() {
-	// An test for how word spacing affects underline and strikeout
-	pdf := fpdf.New("P", "pt", "A4", "")
-	pdf.SetFont("Arial", "", 12)
-	pdf.AddPage()
-	pdf.SetWordSpacing(12)
-	pdf.SetXY(36, 36)
-	pdf.Cell(0, 12, "This is s aimple test to demonstrate word spacing")
-	pdf.SetFont("Arial", "U", 12)
-	pdf.SetXY(36, 60)
-	pdf.Cell(0, 12, "This is s aimple test to demonstrate word spacing")
-	pdf.SetFont("Arial", "S", 12)
-	pdf.SetXY(36, 84)
-	pdf.Cell(0, 12, "This is s aimple test to demonstrate word spacing")
-	fileStr := example.Filename("Fpdf_SetWordSpacing")
-	err := pdf.OutputFileAndClose(fileStr)
-	example.SummaryCompare(err, fileStr)
-}
