b6a675e10f616e64f00e70b5706bd57dcf4b14da
Thierry FOURNIER <tfournier@arpalert.org>
2024-04-04 00:41:49 +0200

    fpdf: draw svg as path
    This patch adds method to draw SVG as path. These paths could
    be filled using standard fpdf methods.
    
    Co-authored-by: Sebastien Binet <binet@cern.ch>

--- svgwrite.go
+++ svgwrite.go
@@ -91,3 +91,68 @@ func (f *Fpdf) SVGBasicWrite(sb *SVGBasicType, scale float64) {
 		}
 	}
 }
+
+// SVGBasicDraw renders the paths in the provided SVGBasicType, but each SVG shape is written
+// as a path that can be filled.
+//
+// styleStr can be "F" for filled, "D" for outlined only, or "DF" or
+// "FD" for outlined and filled. An empty string will be replaced with
+// "D". Drawing uses the current draw color and line width centered on
+// the ellipse's perimeter. Filling uses the current fill color.
+func (f *Fpdf) SVGBasicDraw(sb *SVGBasicType, scale float64, styleStr string) {
+	originX, originY := f.GetXY()
+	var newX, newY float64
+	var cx0, cy0, cx1, cy1 float64
+	var path []SVGBasicSegmentType
+	var seg SVGBasicSegmentType
+	var startX, startY float64
+	if scale == 0.0 {
+		scale = 1.0 / f.k
+	}
+	sval := func(origin float64, arg int) float64 {
+		return origin + scale*seg.Arg[arg]
+	}
+	xval := func(arg int) float64 {
+		return sval(originX, arg)
+	}
+	yval := func(arg int) float64 {
+		return sval(originY, arg)
+	}
+	val := func(arg int) (float64, float64) {
+		return xval(arg), yval(arg + 1)
+	}
+	for j := 0; j < len(sb.Segments) && f.Ok(); j++ {
+		path = sb.Segments[j]
+		for k := 0; k < len(path) && f.Ok(); k++ {
+			seg = path[k]
+			switch seg.Cmd {
+			case 'M':
+				startX, startY = val(0)
+				f.MoveTo(startX, startY)
+			case 'L':
+				newX, newY = val(0)
+				f.LineTo(newX, newY)
+			case 'C':
+				cx0, cy0 = val(0)
+				cx1, cy1 = val(2)
+				newX, newY = val(4)
+				f.CurveBezierCubicTo(cx0, cy0, cx1, cy1, newX, newY)
+			case 'Q':
+				cx0, cy0 = val(0)
+				newX, newY = val(2)
+				f.CurveTo(cx0, cy0, newX, newY)
+			case 'H':
+				newX = xval(0)
+				f.LineTo(newX, f.GetY())
+			case 'V':
+				newY = yval(0)
+				f.LineTo(f.GetX(), newY)
+			case 'Z':
+				f.ClosePath()
+				f.DrawPath(styleStr)
+			default:
+				f.SetErrorf("Unexpected path command '%c'", seg.Cmd)
+			}
+		}
+	}
+}
