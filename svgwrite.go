ad2523fe14f4acd12266a1b415355fd2160f4ff6
Thierry FOURNIER <thierry.f.78@gmail.com>
2024-04-10 13:44:54 +0200

    fpdf: improved support for SVG
    SVG can provide its width and height with unit like 'px', 'pc', 'mm', ...
    The float conversion provided by the XML marshaler returns an error when
    decoding `1.1px` because `px` is not a number. This CL provides a
    method which understands units and applies a `px` conversion.
    
    This CL also adds a method to draw SVGs as paths. These paths could
    then be filled using standard fpdf methods.
    
    Co-authored-by: Sebastien Binet <binet@cern.ch>

--- svgwrite.go
+++ svgwrite.go
@@ -22,7 +22,8 @@ package fpdf
 
 // SVGBasicWrite renders the paths encoded in the basic SVG image specified by
 // sb. The scale value is used to convert the coordinates in the path to the
-// unit of measure specified in New(). The current position (as set with a call
+// unit of measure specified in New(). If scale is 0, SVGBasicWrite automatically adapts the SVG document
+// to the PDF document unit. The current position (as set with a call
 // to SetXY()) is used as the origin of the image. The current line cap style
 // (as set with SetLineCapStyle()), line width (as set with SetLineWidth()),
 // and draw color (as set with SetDrawColor()) are used in drawing the image
@@ -34,6 +35,9 @@ func (f *Fpdf) SVGBasicWrite(sb *SVGBasicType, scale float64) {
 	var path []SVGBasicSegmentType
 	var seg SVGBasicSegmentType
 	var startX, startY float64
+	if scale == 0.0 {
+		scale = 1.0 / f.k
+	}
 	sval := func(origin float64, arg int) float64 {
 		return origin + scale*seg.Arg[arg]
 	}
@@ -81,6 +85,71 @@ func (f *Fpdf) SVGBasicWrite(sb *SVGBasicType, scale float64) {
 			case 'Z':
 				f.Line(x, y, startX, startY)
 				x, y = startX, startY
+			default:
+				f.SetErrorf("Unexpected path command '%c'", seg.Cmd)
+			}
+		}
+	}
+}
+
+// SVGBasicDraw renders the paths in the provided SVGBasicType, but each SVG shape is written
+// as a path that can be filled.
+//
+// styleStr can be "F" for filled, "D" for outlined only, or "DF" or
+// "FD" for outlined and filled. An empty string will be replaced with
+// "D". Drawing uses the current draw color and line width centered on
+// the ellipse's perimeter. Filling uses the current fill color.
+func (f *Fpdf) SVGBasicDraw(sb *SVGBasicType, scale float64, styleStr string) {
+	originX, originY := f.GetXY()
+	var newX, newY float64
+	var cx0, cy0, cx1, cy1 float64
+	var path []SVGBasicSegmentType
+	var seg SVGBasicSegmentType
+	var startX, startY float64
+	if scale == 0.0 {
+		scale = 1.0 / f.k
+	}
+	sval := func(origin float64, arg int) float64 {
+		return origin + scale*seg.Arg[arg]
+	}
+	xval := func(arg int) float64 {
+		return sval(originX, arg)
+	}
+	yval := func(arg int) float64 {
+		return sval(originY, arg)
+	}
+	val := func(arg int) (float64, float64) {
+		return xval(arg), yval(arg + 1)
+	}
+	for j := 0; j < len(sb.Segments) && f.Ok(); j++ {
+		path = sb.Segments[j]
+		for k := 0; k < len(path) && f.Ok(); k++ {
+			seg = path[k]
+			switch seg.Cmd {
+			case 'M':
+				startX, startY = val(0)
+				f.MoveTo(startX, startY)
+			case 'L':
+				newX, newY = val(0)
+				f.LineTo(newX, newY)
+			case 'C':
+				cx0, cy0 = val(0)
+				cx1, cy1 = val(2)
+				newX, newY = val(4)
+				f.CurveBezierCubicTo(cx0, cy0, cx1, cy1, newX, newY)
+			case 'Q':
+				cx0, cy0 = val(0)
+				newX, newY = val(2)
+				f.CurveTo(cx0, cy0, newX, newY)
+			case 'H':
+				newX = xval(0)
+				f.LineTo(newX, f.GetY())
+			case 'V':
+				newY = yval(0)
+				f.LineTo(f.GetX(), newY)
+			case 'Z':
+				f.ClosePath()
+				f.DrawPath(styleStr)
 			default:
 				f.SetErrorf("Unexpected path command '%c'", seg.Cmd)
 			}
